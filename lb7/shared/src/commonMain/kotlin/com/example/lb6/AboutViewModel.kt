901cf5e6aa697c2c2252e2a83d5d9c37896d9706
DimitriuDmytro <dimitriudima28@gmail.com>
2026-06-05 02:21:58 +0300

    lb7

---
+++ lb7/shared/src/commonMain/kotlin/com/example/lb6/AboutViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.lb6
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class AboutViewModel : ViewModel() {
+    // Вказуємо тип <String> явно в кутових дужках
+    private val _platformName = MutableStateFlow<String>(getPlatform().name)
+    val platformName: StateFlow<String> = _platformName
+}
---
+++ lb7/shared/src/commonMain/kotlin/com/example/lb6/App.kt
@@ -0,0 +1,42 @@
+package com.example.lb6
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
+
+@Composable
+fun App(viewModel: AboutViewModel = AboutViewModel()) {
+    // Підписуємось на стан з ViewModel
+    val platformName by viewModel.platformName.collectAsState()
+    var showContent by remember { mutableStateOf(false) }
+
+    AppTheme {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                // Використовуємо дані, отримані через ViewModel
+                Text("Compose: Hello, $platformName!")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
+}
