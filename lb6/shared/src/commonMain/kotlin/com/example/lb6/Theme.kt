563d44bdd016ea8112d3b5a934a8311b38ceb787
DimitriuDmytro <dimitriudima28@gmail.com>
2026-06-05 00:59:13 +0300

    lb6

---
+++ lb6/shared/src/commonMain/kotlin/com/example/lb6/Theme.kt
@@ -0,0 +1,13 @@
+package com.example.lb6
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    val colorScheme = lightColorScheme(
+        primary = PrimaryColor,
+        secondary = SecondaryColor
+    )
+    MaterialTheme(colorScheme = colorScheme, content = content)
+}
